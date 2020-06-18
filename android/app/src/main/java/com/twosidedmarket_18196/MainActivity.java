549dbc72385dec672be9d983ea07aa45c73cf239
Crowdbotics <team@crowdbotics.com>
2020-06-18 17:02:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/twosidedmarket_18196/MainActivity.java
@@ -0,0 +1,15 @@
+package com.twosidedmarket_18196;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "twosidedmarket_18196";
+    }
+}
